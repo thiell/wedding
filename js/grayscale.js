01dfc7c56884cc66e45f8d2bf86fa2a331612d22
Chris Baclig <chris@baclig.me>
2015-10-07 23:41:32 -0500

    Added layouts for next pages

--- js/grayscale.js
+++ js/grayscale.js
@@ -5,13 +5,15 @@
  */
 
 // jQuery to collapse the navbar on scroll
-$(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
-});
+if ($(".navbar.navbar-hidden-top").length) {
+  $(window).scroll(function() {      
+      if ($(".navbar.navbar-hidden-top").offset().top > 50) {
+          $(".navbar-fixed-top").addClass("top-nav-collapse");
+      } else {
+          $(".navbar-fixed-top").removeClass("top-nav-collapse");
+      }
+  });
+}
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
