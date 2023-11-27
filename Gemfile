6b189588f15b71f38eea445c7c889fce0ada362e
Stephane Thiell <sthiell@stanford.edu>
2023-11-26 21:29:14 -0800

    Update

--- Gemfile
+++ Gemfile
@@ -2,4 +2,5 @@
 source "https://rubygems.org"
 
 gem "jekyll"
+gem 'jekyll-multiple-languages-plugin'
 gem 'jekyll-sitemap'
--- js/rsvp.js
+++ js/rsvp.js
@@ -17,3 +17,27 @@ window.addEventListener("load", function() {
     });
   });
 });
+
+$(document).ready(function() {
+  $(".add_item_btn").click(function(e) {
+    e.preventDefault();
+    $("#show_item").prepend(`
+                <div class="row">
+                  <div class="col-md-4 mb-2 col-xs-offset-1">
+                    <input type="text" placeholder="First Name" name="guest_firstname[]" class="form-control" required>
+                  </div>
+                  <div class="col-md-4 mb-1">
+                    <input type="text" placeholder="Last Name" name="guest_lastname[]" class="form-control" required>
+                  </div>
+                  <div class="col-md-2 mb-2 d-grid">
+                    <button class="btn btn-danger remove_item_btn">Remove</button>
+                  </div>
+                </div>
+`)
+  });
+  $(document).on('click', '.remove_item_btn', function(e) {
+    e.preventDefault();
+    let row_item = $(this).parent().parent();
+    $(row_item).remove();
+  });
+});
